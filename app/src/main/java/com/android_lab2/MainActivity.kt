99db01799cb48c3b1bfc2c6f4bcc3bb7dc40cba7
s0m1k111 <spavel1207@gmaiil.com>
2025-11-15 12:43:02 +0300

    ENDDD

--- app/src/main/java/com/android_lab2/MainActivity.kt
+++ app/src/main/java/com/android_lab2/MainActivity.kt
@@ -4,49 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.android_lab2.ui.theme.Android_lab2Theme
+import androidx.compose.ui.unit.sp
+import com.android_lab2.ui.theme.PixLetterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Android_lab2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            PixLetterTheme {
+                GreetingText(message = "Royal Decree!\n" +
+                        "Dear my Friend,\n" +
+                        "His Majesty the King\n" +
+                        "Invites you to a great feast!\n" +
+                        "Come to the castle before sunset,\n" +
+                        "otherwise, you risk losing your head!", from = "From King Arthur", modifier = Modifier.padding(all=8.dp))
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
-        )
+fun RoyalInvitationPreview() {
+    PixLetterTheme {
+        GreetingText(message = "Royal Decree!\n" +
+                "Dear my Friend,\n" +
+                "His Majesty the King\n" +
+                "Invites you to a great feast!\n" +
+                "Come to the castle before sunset,\n" +
+                "otherwise, you risk losing your head!", from = "From King Arthur")
     }
 }
 
-@Preview(showBackground = false)
 @Composable
-fun GreetingPreview() {
-    Android_lab2Theme {
-        Greeting("Android")
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(all=16.dp)
+                .align(alignment = Alignment.End)
+        )
     }
 }
